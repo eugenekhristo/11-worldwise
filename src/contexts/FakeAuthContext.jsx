ed52f8af1b629d251dcb2974691b4f134eadb2a2
Eugene Khristo <marteau9292@gmail.com>
2025-04-12 21:17:17 +0300

    Fake Auth Context

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useReducer } from 'react';
+
+const AuthContext = createContext();
+
+const initialState = { user: null, isAuthenticated: false };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'login':
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case 'logout':
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error('Wrong credentials!');
+  }
+}
+
+const FAKE_USER = {
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz',
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (FAKE_USER.email === email && FAKE_USER.password === password) {
+      dispatch({ type: 'login', payload: FAKE_USER });
+    }
+  }
+
+  function logout() {
+    dispatch({ type: 'logout' });
+  }
+
+  return (
+    <AuthContext.Provider value={(login, logout, user, isAuthenticated)}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const value = useContext(AuthContext);
+  if (value === undefined)
+    throw new Error('AuthContext was used outside AuthProvider');
+  return value;
+}
+
+export { AuthProvider, useAuth };
